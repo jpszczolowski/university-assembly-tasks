ccce266c4984c3666664d0e0a1b0a87f920a2d96
dingorth <dingorth@users.noreply.github.com>
2017-09-17 22:40:14 +0200

    Added comments
    Commenting code is very important.

--- task1/task1.c
+++ task1/task1.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
+#include <stdio.h>	// including headers
+#include <stdlib.h>	// including headers
+#include <stdint.h>	// including headers
 
-int clz(long);
-
+int clz(long);		 
+// this is main function
 int main(int argc, char **argv) {
 	if (argc < 2) {
 		return EXIT_FAILURE;
